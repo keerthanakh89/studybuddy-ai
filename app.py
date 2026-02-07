9036b5fbe3812f078ae65da9a93ba092c1128584
KEERTHANA K H <keerthanakh01@gmail.com>
2026-02-07 22:17:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if not api_key:
 
 # Configure Gemini
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # User inputs
 level = st.selectbox(
